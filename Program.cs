1ebb53f708e45f4188d3573dac6e06501fe091de
Naomysantana <102303751+Naomysantana@users.noreply.github.com>
2022-10-10 17:42:17 -0300

    aplicação para converter as temperaturas.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConversorTemperaturas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Para o usuario digitar a temperatura em celsius fahrenheit e kelvin.
+            double c, f, k;
+
+            Console.WriteLine("### CONVERSOR DE TEMPERATURAS ###"); 
+            Console.Write("Insira a temperatura em Celcius: ");
+            c = double.Parse(Console.ReadLine());
+            Console.WriteLine("------------------------------");
+
+            // Calculo para fahrenheit (c * 9 / 5) + 32 = F
+
+            f = (c * 9 / 5) + 32;
+
+            // Calculo para Kelvin: c + 273,15
+
+            k = c + 273.15;
+
+            Console.WriteLine( c + " graus celcius = " + f + " graus fahrenheit");
+            Console.WriteLine( c + " graus celcius = " + k + " graus kelvin");
+            Console.WriteLine("------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
